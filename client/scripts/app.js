2fc32eca5c9dda988aad36247d351534a7184440
girikalra <girikalra@gmail.com>
2018-01-26 17:10:14 -0800

    Pass 1st events test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,66 @@
-// YOUR CODE HERE:
+// // YOUR CODE HERE:
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+var app = {
+  init: function() {
+    $( '#main .username' ).on('click', app.handleUsernameClick);
+  },
+
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+
+  send: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+
+  fetch: function() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        app.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+
+  clearMessages: function() {
+    $('#chats').empty(); 
+  }, 
+
+  renderMessage: function(message) {
+    $('#chats').append('<div class="username">' + message + '</div>'); 
+  }, 
+
+  renderRoom: function(room) {
+    $('#roomSelect').append('<div>' + room + '</div>');
+  },
+  
+  handleUsernameClick: function() {
+    return true; 
+  } 
+};
+
+
